a875cbfc840f17751ea41a796d705d6bbb1b31a7
damu <damu@example.com>
2020-09-25 15:13:44 +0800

    用户管理模块

--- routers/index.js
+++ routers/index.js
@@ -194,6 +194,71 @@ router.post('/manage/role/update', (req, res) => {
     })
 })
 
+// 添加用户
+router.post('/manage/user/add', (req, res) => {
+  // 读取请求参数数据
+  const {username, password} = req.body
+  // 根据username查询，判断用户是否已经存在，如果存在则返回提示错误的信息，如果不存在则保存
+  UserModel.findOne({username})
+    .then(user => {
+      if (user) {// 如果user有值代表已存在
+        // 返回提示错误的信息
+        res.send({status: 1, msg: '此用户已存在'})
+        return new Promise(() => {
+        })
+      } else { // 无值代表不存在
+        // 保存
+        return UserModel.create({...req.body, password: md5(password || 'admin')})
+      }
+    })
+    .then(user => {
+      // 返回包含user的json数据
+      res.send({status: 0, data: user})
+    })
+    .catch(error => {
+      console.error('注册异常', error)
+      res.send({status: 1, msg: '添加用户异常，请重试'})
+    })
+})
+
+// 获取所有用户列表
+router.get('/manage/user/list', (req, res) => {
+  UserModel.find()
+    .then(users => {
+      RoleModel.find().then(roles => {
+        res.send({status: 0, data: {users, roles}})
+      })
+    })
+    .catch(error => {
+      console.error('获取用户列表异常', error)
+      res.send({status: 1, msg: '获取用户列表异常，请重试'})
+    })
+})
+
+// 更新用户
+router.post('/manage/user/update', (req, res) => {
+  const user = req.body
+  UserModel.findOneAndUpdate({_id: user._id}, user)
+    .then(oldUser => {
+      const data = Object.assign(oldUser, user)
+      // 返回
+      res.send({status: 0, data})
+    })
+    .catch(error => {
+      console.error('更新用户异常', error)
+      res.send({status: 1, msg: '更新用户异常，请重试'})
+    })
+})
+
+// 删除用户
+router.post('/manage/user/delete', (req, res) => {
+  const {userId} = req.body
+  UserModel.deleteOne({_id: userId})
+    .then((doc) => {
+      res.send({status: 0})
+    })
+})
+
 // 得到指定数组的分页信息对象
 function pageFilter(arr, pageNum, pageSize) {
   // 转换数据类型
